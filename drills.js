bfbe50214b0e7ca1929758616081ba3ba6244307
Irem Sen <secilreel@yahoo.com>
2019-04-03 11:48:22 -0400

    second drill

--- drills.js
+++ drills.js
@@ -2,11 +2,13 @@
 
 const memory = require('./Memory');
 
+let Memory = new memory();
+
 class Array {
   constructor() {
     this.length = 0;
     this._capacity = 0;
-    this.ptr = memory.allocate(this.length);
+    this.ptr = Memory.allocate(this.length);
   }
 
   push(value) {
@@ -14,18 +16,18 @@ class Array {
       this._resize((this.length + 1) * Array.SIZE_RATIO);
     }
 
-    memory.set(this.ptr + this.length, value);
+    Memory.set(this.ptr + this.length, value);
     this.length++;
   }
 
   _resize(size) {
     const oldPtr = this.ptr;
-    this.ptr = memory.allocate(size);
+    this.ptr = Memory.allocate(size);
     if (this.ptr === null) {
-      throw new Error('Out of memory');
+      throw new Error('Out of Memory');
     }
-    memory.copy(this.ptr, oldPtr, this.length);
-    memory.free(oldPtr);
+    Memory.copy(this.ptr, oldPtr, this.length);
+    Memory.free(oldPtr);
     this._capacity = size;
   }
 
@@ -33,14 +35,14 @@ class Array {
     if (index < 0 || index >= this.length) {
       throw new Error('Index error');
     }
-    return memory.get(this.ptr + index);
+    return Memory.get(this.ptr + index);
   }
 
   pop() {
     if (this.length == 0) {
       throw new Error('Index error');
     }
-    const value = memory.get(this.ptr + this.length - 1);
+    const value = Memory.get(this.ptr + this.length - 1);
     this.length--;
     return value;
   }
@@ -54,8 +56,8 @@ class Array {
       this._resize((this.length + 1) * Array.SIZE_RATIO);
     }
 
-    memory.copy(this.ptr + index + 1, this.ptr + index, this.length - index);
-    memory.set(this.ptr + index, value);
+    Memory.copy(this.ptr + index + 1, this.ptr + index, this.length - index);
+    Memory.set(this.ptr + index, value);
     this.length++;
   }
 
@@ -63,8 +65,31 @@ class Array {
     if (index < 0 || index >= this.length) {
       throw new Error('Index error');
     }
-    memory.copy(this.ptr + index, this.ptr + index + 1, this.length - index - 1);
+    Memory.copy(this.ptr + index, this.ptr + index + 1, this.length - index - 1);
     this.length--;
   }
 }
 Array.SIZE_RATIO = 3;
+
+function main(){
+
+  Array.SIZE_RATIO = 3;
+
+  // Create an instance of the Array class
+  let arr = new Array();
+
+  // Add an item to the array
+  arr.push(3);
+  arr.push(5);
+  arr.push(15);
+  arr.push(19);
+  arr.push(45);
+  arr.push(10);
+
+  console.log(arr);
+}
+
+main();
+
+// What is the length, capacity and memory address of your array?
+// Array { length: 1, _capacity: 3, ptr: 0 }
